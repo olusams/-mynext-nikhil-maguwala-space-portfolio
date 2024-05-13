85a5e1f970333c8054ff5eb014a756fb36fa8f4d
Olu Sampson <60044354+olusams@users.noreply.github.com>
2024-05-13 11:37:01 +0100

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                     />
 
                     <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-            Nikhil Maguwala
+            Remlyx
           </span>
                 </a>
 
